2afd9afe17d0834b4ae45029b34d3564ccc3b339
daewon-heo <eo7655@naver.com>
2018-07-31 21:54:42 +0900

    prac3 delveop commit
    문제가 무슨말인지 모르겠음

---
+++ src/com/silsub180731/practice3/BookManagerMap.java
@@ -0,0 +1,46 @@
+package com.silsub180731.practice3;
+
+import java.util.HashMap;
+
+import com.silsub180731.practice2.Book;
+
+public class BookManagerMap {
+	private HashMap booksMap;
+	
+	public BookManagerMap() {
+		booksMap = new HashMap();
+	}
+	
+	public BookManagerMap(HashMap booksMap) {
+		this.booksMap = booksMap;
+	}
+	
+	public void putBook(Book book) {
+		
+	}
+	
+	public void removeBook(String key) {
+		
+	}
+	
+	public String searchBook(String bTtitle) {
+		return "";
+	}
+	
+	public void display() {
+		
+	}
+	
+	public Book[] sortedBookMap() {
+		Book[] bs = new Book[2];
+		return bs;
+	}
+	
+	public void printBookMap(Book[] br) {
+		
+	}
+	
+	public void printBook(String key) {
+		
+	}
+}
---
+++ src/com/silsub180731/practice3/TestBookManagerMap.java
@@ -0,0 +1,5 @@
+package com.silsub180731.practice3;
+
+public class TestBookManagerMap {
+
+}
